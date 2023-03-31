a0a3556db18d1cb62990ffe47a0bd27189949773
Ghada <gh.abassi01@gmail.com>
2023-03-31 17:59:27 -0500

    CRUD

--- src/main/java/com/example/demo/gestionConge/exempleEntity/CongeEntityController.java
+++ src/main/java/com/example/demo/gestionConge/exempleEntity/CongeEntityController.java
@@ -1,11 +1,19 @@
 package com.example.demo.gestionConge.exempleEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.example.demo.entities.DemandeConger;
 
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
 @RestController
@@ -17,5 +25,29 @@ public class CongeEntityController {
     public CongeEntityController(CongeEntityServices entityService){
         this.entityService=entityService;
     }
+
+
+	@PostMapping
+    public DemandeConger addDemande(@RequestBody DemandeConger c){
+    	
+    	return entityService.addDemande(c);
+    }
+
+    @GetMapping
+    public List<DemandeConger> GetAllDemande(){
+    	
+		return entityService.GetAllDemande();
+	}
+
+    @PutMapping
+    public DemandeConger updateDemande(@RequestBody DemandeConger c ,@PathVariable Long idDemandeConger ){
+    	
+    	
+		return entityService.updateDemande(c, idDemandeConger);}
+
+    @DeleteMapping(path="{idDemandeConger}")
+    public String deleteDemande(@PathVariable Long idDemandeConger){
+    	
+		return entityService.deleteDemande(idDemandeConger);}
 
 }
--- src/main/java/com/example/demo/gestionConge/exempleEntity/CongeEntityServices.java
+++ src/main/java/com/example/demo/gestionConge/exempleEntity/CongeEntityServices.java
@@ -2,9 +2,96 @@ package com.example.demo.gestionConge.exempleEntity;
 
 
 import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.demo.Doa.DemandeCongerRepository;
+import com.example.demo.entities.DemandeConger;
+import com.example.demo.entities.Employer;
+import com.example.demo.entities.Enseignant;
+import com.example.demo.entities.StaffAdministratif;
+
+
 
 @Service
 @AllArgsConstructor
 public class CongeEntityServices {
+	
+	@Autowired
+	private  DemandeCongerRepository demandeCongerRepository;
+	
+    public DemandeConger addDemande(@RequestBody DemandeConger c){
+    	
+        if (c == null || c.getDateDebut() == null || c.getDateFin() == null || c.getJustification() == null || c.getType() ==null || c.getTypecongé()==null) {
+            throw new IllegalArgumentException("La demande est invalide");
+        }
+
+        if (c.getDateDebut().after(c.getDateFin())) {
+            throw new IllegalArgumentException("La date de début doit être avant la date de fin");
+        }
+       
+        Employer employer = c.getEmployer();
+        
+        long differenceMillis = Math.abs(c.getDateFin().getTime() - c.getDateDebut().getTime());
+
+     
+        long nbJoursDemande = TimeUnit.DAYS.convert(differenceMillis, TimeUnit.MILLISECONDS);
+        
+        if(employer.getNbJourCongeRestant() <nbJoursDemande) {
+        	 throw new IllegalArgumentException("Nombre de Jours demande est superieure a celui de jours restants ");
+        }
+        
+        if(employer instanceof Enseignant) {
+        	
+        	
+        	
+        }
+        if(employer instanceof StaffAdministratif) {
+        	
+        	
+        }
+        
+        employer.addDemandeConger(c);
+    	
+    	return demandeCongerRepository.save(c);
+    }
+    
+    
+    public List<DemandeConger> GetAllDemande(){
+    	
+		return demandeCongerRepository.findAll();
+	}
+
+    public DemandeConger updateDemande(@RequestBody DemandeConger c ,@PathVariable Long idDemandeConger ){
+    	
+		 return demandeCongerRepository.findById(idDemandeConger).map(conge ->{
+			c.setType(c.getType());
+			c.setTypecongé(c.getTypecongé());
+			c.setJustification(c.getJustification());
+			c.setDateDebut(c.getDateDebut());
+			c.setDateFin(c.getDateFin());
+			//c.setEtatDemande(c.getEtatDemande());
+			//c.setEmployer(c.getEmployer());
+			
+			return conge;
+			
+		}).orElseThrow(() -> new ResourceNotFoundException("Demande not exist with id: " + idDemandeConger) );}
+	
+    
+    public String deleteDemande(@PathVariable Long idDemandeConger){
+		if(! demandeCongerRepository.existsById(idDemandeConger)) {
+			return "Demande not exist with id: " + idDemandeConger;
+		}
+		demandeCongerRepository.deleteById(idDemandeConger);
+		return "Demande deleted with success !";	
+    	
+		}
+
 }
